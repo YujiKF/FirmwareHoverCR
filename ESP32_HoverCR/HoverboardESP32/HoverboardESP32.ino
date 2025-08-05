fda467e145f506a700bf5f6f8f775b4570a89a31
YujiKF <yujikf@usp.br>
2025-08-05 11:14:10 -0300

    Revisando firmware da ESP32 Hoverboard

--- ESP32_HoverCR/HoverboardESP32/HoverboardESP32.ino
+++ ESP32_HoverCR/HoverboardESP32/HoverboardESP32.ino
@@ -6,6 +6,7 @@
  *
  * Este código é para a ESP32 que fica conectada ao HOVERBOARD. O pino 16 da ESP32 (RX) deve ser conectado ao pino TX do Hoverboard, 
  assim como o pino 17 da ESP32 (TX) deve ser conectado ao pino RX do Hoverboard, além disso, os GNDs devem ser conectados também.
+ * A função desse código é receber comandos do Controle Remoto, não sendo capaz de receber pelo Serial Monitor do Arduino IDE ou por Bluetooth, por exemplo.
  * ====================================================================
 */
 
@@ -13,117 +14,8 @@
 #include <esp_now.h>
 #include <WiFi.h>
 #include <esp_wifi.h>
-
-template <typename O,typename I> void HoverSetupEsp32(O& oSerial, I iBaud, I gpio_RX, I gpio_TX)
-{
-  // Inicia a comunicação serial usando baudrate, protocolo, GPIO RX, GPIO TX.
-  // Números de GPIO da placa ESP32.
-  oSerial.begin(iBaud, SERIAL_8N1, gpio_RX, gpio_TX);
-}
-
-uint16_t CalcCRC(uint8_t *ptr, int count)
-{
-  uint16_t  crc;
-  uint8_t i;
-  crc = 0;
-  while (--count >= 0)
-  {
-    crc = crc ^ (uint16_t) *ptr++ << 8;
-    i = 8;
-    do
-    {
-      if (crc & 0x8000)
-      {
-        crc = crc << 1 ^ 0x1021;
-      }
-      else
-      {
-        crc = crc << 1;
-      }
-    } while(--i);
-  }
-  return (crc);
-}
-
-#define START_FRAME         0xABCD       // Definição de start frame para comunicação serial
-
-typedef struct __attribute__((packed, aligned(1))) {
-    uint16_t cStart = START_FRAME;
-    int16_t iSpeedL;
-    int16_t iSpeedR;
-    uint16_t iVolt;
-    int16_t iAmpL;
-    int16_t iAmpR;
-    int32_t iOdomL;
-    int32_t iOdomR;
-    uint16_t checksum;
-} SerialHover2Server;
-
-typedef struct __attribute__((packed, aligned(1))) {
-    uint8_t cStart = '/';
-    int16_t  iSpeed = 0;
-    int16_t  iSteer = 0;
-    uint8_t  wStateMaster = 0;
-    uint8_t  wStateSlave = 0;
-    float Kp = 0;
-    float Ki = 0;
-    float Kd = 0;
-    uint16_t checksum;
-} SerialServer2Hover;
-
-template <typename O,typename I> void HoverSend(O& oSerial, I iSteer, I iSpeed,uint8_t  wStateMaster=32, uint8_t  wStateSlave=0, float Kp = 0, float Ki = 0, float Kd = 0)
-{
-  SerialServer2Hover oData;
-  oData.iSpeed    = (int16_t)iSpeed;
-  oData.iSteer    = (int16_t)iSteer;
-  oData.wStateMaster  = wStateMaster;
-  oData.wStateSlave   = wStateSlave;
-  oData.Kp = Kp;
-  oData.Ki = Ki;
-  oData.Kd = Kd;
-  oData.checksum = CalcCRC((uint8_t*)&oData, sizeof(SerialServer2Hover)-2);
-  oSerial.write((uint8_t*) &oData, sizeof(SerialServer2Hover)); 
-}
-
-template <typename O,typename OF> boolean Receive(O& oSerial, OF& Feedback)
-{
-  int iTooMuch = oSerial.available() - sizeof(SerialHover2Server) + 1;
-  int8_t bFirst = 1;
-  while (iTooMuch >= bFirst )
-  {
-    byte c = oSerial.read();
-    iTooMuch--;
-    
-    if (bFirst) 
-    {
-      if (c == (byte)START_FRAME)
-      {
-        bFirst = 0;
-      }
-    }
-    else
-    {
-      if (c == START_FRAME >>8 )
-      {
-        SerialHover2Server tmpFeedback;
-        byte* p = (byte *)&tmpFeedback+2;
-        for (int i = sizeof(SerialHover2Server); i>2; i--)  
-          *p++    = oSerial.read();
-
-        uint16_t checksum = CalcCRC((byte *)&tmpFeedback, sizeof(SerialHover2Server)-2);
-        if (checksum == tmpFeedback.checksum)
-        {
-            memcpy(&Feedback, &tmpFeedback, sizeof(SerialHover2Server));
-            return true;
-        }
-        return false;       
-      }
-      if (c != (byte)START_FRAME)
-        bFirst = 1;
-    }
-  }
-  return false;
-}
+#include "hoverserial.h"
+#include "util.h"
 
 // =================================================================================
 // |                INÍCIO DA LÓGICA PRINCIPAL DO HOVERBOARD                       |
@@ -149,9 +41,9 @@ SerialHover2Server oHoverFeedback;
 esp_now_peer_info_t peerInfo;
 bool isRemotePaired = false;
 volatile int iSpeed = 0;
-volatile float Kp_s = 20.0f;
-volatile float Ki_s = 10.0f;
-volatile float Kd_s = 0.5f;
+volatile float Kp_s = 30.0f;
+volatile float Ki_s = 80.0f;
+volatile float Kd_s = 5.0f;
 unsigned long lastSendTime = 0, lastLogTime = 0, lastCmdReceivedTime = 0;
 
 // --- FUNÇÃO DE CALLBACK (ESP-NOW) ---
@@ -215,7 +107,7 @@ void loop() {
   if (Receive(SerialHover, oHoverFeedback)) {
     // Envia a telemetria de volta para o controle remoto
     if (iNow - lastLogTime > 100) {
-      telemetryData.real_speed_kmh = oHoverFeedback.iSpeedL / 100.0;
+      telemetryData.real_speed_kmh = -oHoverFeedback.iSpeedL / 100.0;
       telemetryData.battery_volt = oHoverFeedback.iVolt / 100.0;
       
       if (isRemotePaired) {
